c706b454c22535326144d866a63ac3f05ac9e63f
jgonzalezf07 <jgonzalezf07@educarex.es>
2026-02-05 12:06:59 +0100

    Add files via upload

---
+++ Shell Scripts/Calculadora.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#Autor: Jesús González Folleco
+#Fecha:04/02/2026
+
+clear
+
+echo "Jesús González Folleco"
+
+
+if [ -f $1 ]; then
+
+        echo "El fichero existe"
+        exit
+
+fi
+
+if [ $# -ne 2 ]; then
+
+	echo "No has introducido dos parámetros"
+	exit
+fi
+
+
+for ((contador=0; contador<=$2; contador++)); do
+
+read -p "Dime que operación quieres hacer: " op op1 op2
+
+case $op in
+
+	"S")
+		res=$(($op1+$op2))
+		echo "$op1 + $op2 = $res"
+		echo "$op $op1 $op2" >>$1
+
+	;;
+
+	"R")
+                res=$(($op1 - $op2))
+                echo "$op1 - $op2 = $res "
+		echo "$op $op1 $op2" >>$1
+
+        ;;
+
+	"M")
+                res=$(($op1*$op2))
+                echo "$op1 * $op2 = $res "
+		echo "$op $op1 $op2" >>$1
+
+        ;;
+
+	"D")
+                res=$(($op1/$op2))
+                echo "$op1 / $op2 = $res "
+		echo "$op $op1 $op2" >>$1 
+        ;;
+
+	"X")
+		echo "Saliendo..."
+		exit
+	;;
+
+	"B")
+		>$1
+		echo "El fichero se ha vaciado correctamente"
+	;;
+
+	*)
+		echo "Error al dar la orden..."
+		break
+	;;
+esac
+
+	echo "Has realizado $contador operaciones"
+	cat $1
+
+done
